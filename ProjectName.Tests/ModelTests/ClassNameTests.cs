92e693a46eb619759a7473953d0e9517a9a52cf5
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-12 21:34:58 -0700

    Add example tests.

--- ProjectName.Tests/ModelTests/ClassNameTests.cs
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -12,5 +12,35 @@ namespace ProjectName.Tests
 //   // any necessary logic to prep for test; instantiating new classes, etc.
 //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
   //}
+    // [TestMethod]
+    // public void ItemConstructor_CreatesInstanceOfItem_Item()
+    // {
+    //   Item newItem = new Item();
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+//     [TestMethod]
+//     public void GetDescription_ReturnsDescription_String()
+//     {
+//       string description = "Walk the dog.";
+//       Item newItem = new Item(description);
+//       string result = newItem.Description;
+//       Assert.AreEqual(description, result);
+//     }
+
+//     [TestMethod]
+// public void GetDescription_ReturnsDescription_String()
+// {
+//   //Arrange
+//   string description = "Walk the dog.";
+//   Item newItem = new Item(description);
+
+//   //Act
+//   string result = newItem.Description;
+
+//   //Assert
+//   Assert.AreEqual(description, result);
+// }
+
   }
 }
